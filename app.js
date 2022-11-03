b7d9c4dfaccb2ff3877d6acbe90455f5e31d2c87
nyanpasa <87476811420x@gmail.com>
2022-11-03 19:21:00 +0600

    Revert "add new repo"
    This reverts commit 005aca240d28b5685f7ed9a42009affa24eecbfa.

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const morgan = require('morgan');
-
-const app = express();
-
-app.use(express.json());
-app.use(morgan('dev'));
-
-module.exports = app;
--- server.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const app = require('./app');
-
-const port = 3000;
-app.listen(port, () => {});
