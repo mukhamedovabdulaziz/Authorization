3098e9c9e40c3bcc3c4b6843269eaa0408c2c0ba
Abdulaziz <mukhamedovabdulaziz@gmail.com>
2022-11-25 11:51:44 +0600

    divine components by tabs and searchbar

--- src/components/Simulator/Content/Content.jsx
+++ src/components/Simulator/Content/Content.jsx
@@ -1,58 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import s from "./Content.module.css";
+import Tabs from "./Tabs/Tabs";
+import SearchBar from "./SearchBar/SearchBar";
 
 function Content() {
 
-  const [isActive, setIsActive] = useState(true);
-
-  const handleClick = () => {
-    setIsActive(current => !current);
-  };
-
-  const cifStyle = {
-    backgroundColor: isActive ? "#008fff" : "#82c8ff"
-  }
-
-  const dapStyle = {
-    backgroundColor: isActive? "#82c8ff" : "#008fff"
-  }
-
   return (
     <section className={s.content}>
-      <div className={s.tabs}>
-        <button type="button" className={s.cif} style={cifStyle} onClick={handleClick}>CIF</button>
-        <button type="button" className={s.dap} style={dapStyle} onClick={handleClick}>DAP</button>
-      </div>
-
-      <div className={s.searchBar}>
-        <div className={s.inputs}>
-          <div className={s.allInput}>
-            <div>
-              <label className={s.simLabel} htmlFor="port_of_shipment">Port of Shipment
-                <input className={s.simInput} id="port_of_shipment"/>
-              </label>
-            </div>
-            <div>
-              <label className={s.simLabel} htmlFor="destination">Destination
-                <input className={s.simInput} id="destination"/>
-              </label>
-            </div>
-            <div>
-              <label className={s.simLabel} htmlFor="date">Date
-                <input className={s.simInput} id="date"/>
-              </label>
-            </div>
-            <div>
-              <label className={s.simLabel} htmlFor="container_types">Container Types
-                <input className={s.simInput} id="container_types"/>
-              </label>
-            </div>
-          </div>
-        </div>
-        <button type="button" className={s.search} id="search" style={{fontWeight: "bold"}}>Search</button>
-      </div>
+      <Tabs/>
+      <SearchBar/>
     </section>
   );
-};
+}
 
 export default Content;
---
+++ src/components/Simulator/Content/SearchBar/SearchBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import s from "./SearchBar.module.css";
+
+function SearchBar() {
+  return (
+    <div>
+      <div className={s.searchBar}>
+        <div className={s.inputs}>
+          <div className={s.allInput}>
+            <div>
+              <label className={s.simLabel} htmlFor="port_of_shipment">Port of Shipment
+                <input className={s.simInput} id="port_of_shipment"/>
+              </label>
+            </div>
+            <div>
+              <label className={s.simLabel} htmlFor="destination">Destination
+                <input className={s.simInput} id="destination"/>
+              </label>
+            </div>
+            <div>
+              <label className={s.simLabel} htmlFor="date">Date
+                <input className={s.simInput} id="date"/>
+              </label>
+            </div>
+            <div>
+              <label className={s.simLabel} htmlFor="container_types">Container Types
+                <input className={s.simInput} id="container_types"/>
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="button" className={s.search} id="search" style={{fontWeight: "bold"}}>Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
